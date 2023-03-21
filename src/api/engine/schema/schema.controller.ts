fa4a81c90f30048dab74cdc736fb3de424644b8a
Kiran Kharade <kiran.kharade@inflectionzone.com>
2023-03-21 13:20:33 +0530

    Added schema pipeline

--- src/api/engine/schema/schema.controller.ts
+++ src/api/engine/schema/schema.controller.ts
@@ -1,82 +1,90 @@
-// import express from 'express';
-// import {
-//     ResponseHandler
-// } from '../../../common/response.handler';
-// import {
-//     SchemaControllerDelegate
-// } from './schema.controller.delegate';
-// import {
-//     BaseController
-// } from '../../base.controller';
+import express from 'express';
+import { ResponseHandler } from '../../../common/handlers/response.handler';
+import { SchemaValidator } from './schema.validator';
+import { BaseController } from '../../base.controller';
+import { SchemaService } from '../../../database/services/engine/schema.service';
+import { ErrorHandler } from '../../../common/handlers/error.handler';
+import { SchemaCreateModel, SchemaSearchFilters, SchemaUpdateModel } from '../../../domain.types/engine/schema.domain.types';
+import { uuid } from '../../../domain.types/miscellaneous/system.types';
 
-// ///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
 
-// export class SchemaController extends BaseController {
+export class SchemaController extends BaseController {
 
-//     //#region member variables and constructors
+    //#region member variables and constructors
 
-//     _delegate: SchemaControllerDelegate = null;
+    _service: SchemaService = new SchemaService();
 
-//     constructor() {
-//         super();
-//         this._delegate = new SchemaControllerDelegate();
-//     }
+    _validator: SchemaValidator = new SchemaValidator();
 
-//     //#endregion
+    constructor() {
+        super();
+    }
 
-//     create = async (request: express.Request, response: express.Response): Promise < void > => {
-//         try {
-//             await this.authorize('Scheme.Create', request, response);
-//             const record = await this._delegate.create(request.body);
-//             const message = 'Scheme added successfully!';
-//             ResponseHandler.success(request, response, message, 201, record);
-//         } catch (error) {
-//             ResponseHandler.handleError(request, response, error);
-//         }
-//     }
+    //#endregion
 
-//     getById = async (request: express.Request, response: express.Response): Promise < void > => {
-//         try {
-//             await this.authorize('Scheme.GetById', request, response);
-//             const record = await this._delegate.getById(request.params.id);
-//             const message = 'Scheme retrieved successfully!';
-//             ResponseHandler.success(request, response, message, 200, record);
-//         } catch (error) {
-//             ResponseHandler.handleError(request, response, error);
-//         }
-//     }
+    create = async (request: express.Request, response: express.Response) => {
+        try {
+            await this.authorize('Schema.Create', request, response);
+            var model: SchemaCreateModel = await this._validator.validateCreateRequest(request);
+            const record = await this._service.create(model);
+            if (record === null) {
+                ErrorHandler.throwInternalServerError('Unable to add schema!');
+            }
+            const message = 'Schema added successfully!';
+            return ResponseHandler.success(request, response, message, 201, record);
+        } catch (error) {
+            ResponseHandler.handleError(request, response, error);
+        }
+    };
 
-//     search = async (request: express.Request, response: express.Response): Promise < void > => {
-//         try {
-//             await this.authorize('Scheme.Search', request, response);
-//             const searchResults = await this._delegate.search(request.query);
-//             const message = 'Scheme records retrieved successfully!';
-//             ResponseHandler.success(request, response, message, 200, searchResults);
-//         } catch (error) {
-//             ResponseHandler.handleError(request, response, error);
-//         }
-//     }
+    getById = async (request: express.Request, response: express.Response) => {
+        try {
+            await this.authorize('Schema.GetById', request, response);
+            var id: uuid = await this._validator.validateParamAsUUID(request, 'id');
+            const record = await this._service.getById(id);
+            const message = 'Schema retrieved successfully!';
+            return ResponseHandler.success(request, response, message, 200, record);
+        } catch (error) {
+            ResponseHandler.handleError(request, response, error);
+        }
+    };
 
-//     update = async (request: express.Request, response: express.Response): Promise < void > => {
-//         try {
-//             await this.authorize('Scheme.Update', request, response);
-//             const updatedRecord = await this._delegate.update(request.params.id, request.body);
-//             const message = 'Scheme updated successfully!';
-//             ResponseHandler.success(request, response, message, 200, updatedRecord);
-//         } catch (error) {
-//             ResponseHandler.handleError(request, response, error);
-//         }
-//     }
+    update = async (request: express.Request, response: express.Response) => {
+        try {
+            await this.authorize('Schema.Update', request, response);
+            const id = await this._validator.validateParamAsUUID(request, 'id');
+            var model: SchemaUpdateModel = await this._validator.validateUpdateRequest(request);
+            const updatedRecord = await this._service.update(id, model);
+            const message = 'Schema updated successfully!';
+            ResponseHandler.success(request, response, message, 200, updatedRecord);
+        } catch (error) {
+            ResponseHandler.handleError(request, response, error);
+        }
+    };
 
-//     delete = async (request: express.Request, response: express.Response): Promise < void > => {
-//         try {
-//             await this.authorize('Scheme.Delete', request, response);
-//             const result = await this._delegate.delete(request.params.id);
-//             const message = 'Scheme deleted successfully!';
-//             ResponseHandler.success(request, response, message, 200, result);
-//         } catch (error) {
-//             ResponseHandler.handleError(request, response, error);
-//         }
-//     };
+    search = async (request: express.Request, response: express.Response) => {
+        try {
+            await this.authorize('Schema.Search', request, response);
+            var filters: SchemaSearchFilters = await this._validator.validateSearchRequest(request);
+            const searchResults = await this._service.search(filters);
+            const message = 'Schema records retrieved successfully!';
+            ResponseHandler.success(request, response, message, 200, searchResults);
+        } catch (error) {
+            ResponseHandler.handleError(request, response, error);
+        }
+    };
 
-// }
+    delete = async (request: express.Request, response: express.Response): Promise < void > => {
+        try {
+            await this.authorize('Schema.Delete', request, response);
+            var id: uuid = await this._validator.validateParamAsUUID(request, 'id');
+            const result = await this._service.delete(id);
+            const message = 'Schema deleted successfully!';
+            ResponseHandler.success(request, response, message, 200, result);
+        } catch (error) {
+            ResponseHandler.handleError(request, response, error);
+        }
+    };
+
+}
---
+++ src/database/services/engine/schema.service.ts
@@ -0,0 +1,188 @@
+import { Schema } from '../../models/engine/schema.model';
+import { Client } from '../../models/client/client.model';
+import { logger } from '../../../logger/logger';
+import { ErrorHandler } from '../../../common/handlers/error.handler';
+import { Source } from '../../../database/database.connector';
+import { FindManyOptions, Like, Repository } from 'typeorm';
+import { SchemaMapper } from '../../mappers/engine/schema.mapper';
+import { BaseService } from '../base.service';
+import { uuid } from '../../../domain.types/miscellaneous/system.types';
+import {
+    SchemaCreateModel,
+    SchemaResponseDto,
+    SchemaSearchFilters,
+    SchemaSearchResults,
+    SchemaUpdateModel } from '../../../domain.types/engine/schema.domain.types';
+
+///////////////////////////////////////////////////////////////////////
+
+export class SchemaService extends BaseService {
+
+    //#region Repositories
+
+    _schemaRepository: Repository<Schema> = Source.getRepository(Schema);
+
+    _clientRepository: Repository<Client> = Source.getRepository(Client);
+
+    //#endregion
+
+    public create = async (createModel: SchemaCreateModel)
+        : Promise<SchemaResponseDto> => {
+
+        const client = await this.getClient(createModel.ClientId);
+        const schema = this._schemaRepository.create({
+            Client      : client,
+            Name        : createModel.Name,
+            Description : createModel.Description,
+            ValidFrom   : createModel.ValidFrom,
+            ValidTill   : createModel.ValidTill,
+            IsValid     : createModel.IsValid ?? true,
+        });
+        var record = await this._schemaRepository.save(schema);
+        return SchemaMapper.toResponseDto(record);
+    };
+
+    public getById = async (id: uuid): Promise<SchemaResponseDto> => {
+        try {
+            var schema = await this._schemaRepository.findOne({
+                where : {
+                    id : id
+                }
+            });
+            return SchemaMapper.toResponseDto(schema);
+        } catch (error) {
+            logger.error(error.message);
+            ErrorHandler.throwInternalServerError(error.message, 500);
+        }
+    };
+
+    public search = async (filters: SchemaSearchFilters)
+        : Promise<SchemaSearchResults> => {
+        try {
+            var search = this.getSearchModel(filters);
+            var { search, pageIndex, limit, order, orderByColumn } = this.addSortingAndPagination(search, filters);
+            const [list, count] = await this._schemaRepository.findAndCount(search);
+            const searchResults = {
+                TotalCount     : count,
+                RetrievedCount : list.length,
+                PageIndex      : pageIndex,
+                ItemsPerPage   : limit,
+                Order          : order === 'DESC' ? 'descending' : 'ascending',
+                OrderedBy      : orderByColumn,
+                Items          : list.map(x => SchemaMapper.toResponseDto(x)),
+            };
+            return searchResults;
+        } catch (error) {
+            logger.error(error.message);
+            ErrorHandler.throwDbAccessError('DB Error: Unable to search api schema records!', error);
+        }
+    };
+
+    public update = async (id: uuid, model: SchemaUpdateModel)
+        : Promise<SchemaResponseDto> => {
+        try {
+            const schema = await this._schemaRepository.findOne({
+                where : {
+                    id : id
+                }
+            });
+            if (!schema) {
+                ErrorHandler.throwNotFoundError('Schema not found!');
+            }
+            //Schema code is not modifiable
+            //Use renew key to update ApiKey, ValidFrom and ValidTill
+
+            if (model.ClientId != null) {
+                const client = await this.getClient(model.ClientId);
+                schema.Client = client;
+            }
+            if (model.Name != null) {
+                schema.Name = model.Name;
+            }
+            if (model.Description != null) {
+                schema.Description = model.Description;
+            }
+            if (model.ValidFrom != null) {
+                schema.ValidFrom = model.ValidFrom;
+            }
+            if (model.ValidTill != null) {
+                schema.ValidTill = model.ValidTill;
+            }
+            if (model.IsValid != null) {
+                schema.IsValid = model.IsValid;
+            }
+            var record = await this._schemaRepository.save(schema);
+            return SchemaMapper.toResponseDto(record);
+        } catch (error) {
+            logger.error(error.message);
+            ErrorHandler.throwInternalServerError(error.message, 500);
+        }
+    };
+
+    public delete = async (id: string): Promise<boolean> => {
+        try {
+            var record = await this._schemaRepository.findOne({
+                where : {
+                    id : id
+                }
+            });
+            var result = await this._schemaRepository.remove(record);
+            return result != null;
+        } catch (error) {
+            logger.error(error.message);
+            ErrorHandler.throwInternalServerError(error.message, 500);
+        }
+    };
+
+    //#region Privates
+
+    private getSearchModel = (filters: SchemaSearchFilters) => {
+
+        var search : FindManyOptions<Schema> = {
+            relations : {
+            },
+            where : {
+            },
+            select : {
+                id     : true,
+                Client : {
+                    id   : true,
+                    Name : true,
+                    Code : true,
+                },
+                Name        : true,
+                Description : true,
+                RootNodeId  : true,
+                ValidFrom   : true,
+                ValidTill   : true,
+                IsValid     : true,
+                CreatedAt   : true,
+                UpdatedAt   : true,
+            }
+        };
+
+        if (filters.ClientId) {
+            search.where['Client'].id = filters.ClientId;
+        }
+        if (filters.Name) {
+            search.where['Name'] = Like(`%${filters.Name}%`);
+        }
+
+        return search;
+    };
+
+    //#endregion
+
+    private async getClient(clientId: uuid) {
+        const client = await this._clientRepository.findOne({
+            where : {
+                id : clientId
+            }
+        });
+        if (!client) {
+            ErrorHandler.throwNotFoundError('Client cannot be found');
+        }
+        return client;
+    }
+
+}
