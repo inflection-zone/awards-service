39b16243fccff3c7f579f48b8bc2503f720d949f
Kiran Kharade <Kiran.Kharade@inflectionzone.com>
2023-04-03 23:46:28 +0530

    Updated condition route

--- src/api/engine/condition/condition.routes.ts
+++ src/api/engine/condition/condition.routes.ts
@@ -14,11 +14,11 @@ export const register = (app: express.Application): void => {
     const authenticator = Loader.Authenticator;
     const controller = new ConditionController();
 
-    router.post('/', authenticator.authenticateClient, controller.create);
-    router.get('/search', authenticator.authenticateClient, controller.search);
-    router.get('/:id', authenticator.authenticateClient, controller.getById);
-    router.put('/:id', authenticator.authenticateClient, controller.update);
-    router.delete('/:id', authenticator.authenticateClient, controller.delete);
+    router.post('/', authenticator.authenticateClient, authenticator.authenticateUser, controller.create);
+    router.get('/search', authenticator.authenticateClient, authenticator.authenticateUser, controller.search);
+    router.get('/:id', authenticator.authenticateClient, authenticator.authenticateUser, controller.getById);
+    router.put('/:id', authenticator.authenticateClient, authenticator.authenticateUser, controller.update);
+    router.delete('/:id', authenticator.authenticateClient, authenticator.authenticateUser, controller.delete);
 
     app.use('/api/v1/engine/conditions', router);
 };
