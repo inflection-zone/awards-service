aa35fdaad01442920a2a684fc387738f8ff333c7
RupaliD <rupali.dinde@inflectionzone.com>
2023-10-04 13:49:22 +0530

    Removed the pagination changes from badge get all api

--- src/api/awards/badge/badge.controller.ts
+++ src/api/awards/badge/badge.controller.ts
@@ -56,8 +56,7 @@ export class BadgeController extends BaseController {
     getAll = async (request: express.Request, response: express.Response) => {
         try {
             await this.authorize('Badge.GetAll', request, response, false);
-            var filters: BadgeSearchFilters = this.getSearchFilters(request.query);
-            const records = await this._service.search(filters);
+            const records = await this._service.search({});
             const message = 'Badge records with how to earn content retrieved successfully!';
             ResponseHandler.success(request, response, message, 200, records);
         } catch (error) {
@@ -115,34 +114,5 @@ export class BadgeController extends BaseController {
             ResponseHandler.handleError(request, response, error);
         }
     };
-
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-
-    getSearchFilters = (query) => {
-
-        var filters = {};
-
-        var categoryId = query.categoryId ? query.categoryId : null;
-        if (categoryId != null) {
-            filters['CategoryId'] = categoryId;
-        }
-        var name = query.name ? query.name : null;
-        if (name != null) {
-            filters['Name'] = name;
-        }
-        var clientId = query.clientId ? query.clientId : null;
-        if (clientId != null) {
-            filters['ClientId'] = clientId;
-        }
-        var orderBy = query.orderBy ? query.orderBy : 'CreatedAt';
-        if (orderBy != null) {
-            filters['OrderBy'] = orderBy;
-        }
-        var order = query.order ? query.order : 'ASC';
-        if (order != null) {
-            filters['Order'] = order;
-        }
-        return filters;
-    };
-
+    
 }
